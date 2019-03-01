8977311c9e8c8562d7541ba0bf8125564dbe0cfb
JamesMusyoka <jamesmu475@gmail.com>
2019-03-01 11:43:01 +0300

    test some models in the file

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Location,Image,Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.nairobi= Location(first_name = 'Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+     # Testing Save Method
+    def test_save_method(self):
+        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    
+    def nairobi.save_location(self):
+        self.save()
