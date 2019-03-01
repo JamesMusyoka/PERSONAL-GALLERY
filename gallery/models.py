cf6b36b3636fb930efe20ac0f23d56fa887a9178
JamesMusyoka <jamesmu475@gmail.com>
2019-03-01 11:42:28 +0300

    Added few class in the file

--- gallery/models.py
+++ gallery/models.py
@@ -6,3 +6,23 @@ class Location(models.Model):
 
     def __str__(self):
         return self.first_name
+
+class Category(models.Model):
+    name = models.CharField(max_length =25)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to ='gallery/')
+    image_name = models.CharField(max_length =20)
+    image_description = models.TextField(max_length =250)
+    image_location = models.ForeignKey(Location)
+    image_category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.image_name
+
+    class Meta:
+        ordering = ['image_name']
