a986b539445ef716983ec9c2d15af77c9dffb37b
JamesMusyoka <jamesmu475@gmail.com>
2019-03-02 10:48:54 +0300

    initialiased

--- personalG/urls.py
+++ personalG/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^gallery/',include('gallery.urls')),
-    # url(r'^search/',views.search_results,name = 'search_results'),
+    url(r'',include('gallery.urls')),
 ]
