377da4e6320818d570b8e091b6f26d525c86603f
JamesMusyoka <jamesmu475@gmail.com>
2019-03-02 11:28:02 +0300

    added a class to make our test  pass

--- gallery/views.py
+++ gallery/views.py
@@ -21,6 +21,16 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
+
 def search_results(request):
-    raise Http404()  
-    return render(request, 'templates/search_results.html')
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+
+        message = f"{search_term}"
+        return render(request, 'search.html', {"message":message, "images": searched_images})
+
+    else:
+        message = "You haven't made any searches"
+        return render(request, 'search.html', {"message":message})
