33c0ba272aaab29eccc1a2fc8cf62135adb46feb
JamesMusyoka <jamesmu475@gmail.com>
2019-03-02 10:48:00 +0300

    added  codes for more functionalityand test

--- gallery/tests.py
+++ gallery/tests.py
@@ -18,6 +18,65 @@ class LocationTestClass(TestCase):
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
 
+    def delete_save_method(self):
+        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
+
+
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.fashion= Category(name = 'Fashion')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.fashion,Category))
+
+     # Testing Save Method
+    def test_save_method(self):
+        self.fashion.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def delete_save_method(self):
+        self.fashion.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        nairobi = Location(first_name='nairobi')
+        nairobi.save()
+        fashion= Category(name = 'Fashion')
+        fashion.save()
+
+        self.image= Image(image = 'jpg', image_name= 'image', image_description= 'hotels', image_location= nairobi,image_category=fashion)
+        self.image.save()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+     # Testing Save Method
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def delete_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
     
-    def nairobi.save_location(self):
-        self.save()
+   
