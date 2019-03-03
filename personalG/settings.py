0f01971be586dc4bb69d3020bae99989e4e89130
JamesMusyoka <jamesmu475@gmail.com>
2019-03-03 15:42:49 +0300

    Added a url link

--- personalG/settings.py
+++ personalG/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'personalG.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+             'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -65,11 +65,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = 'personalG.wsgi.application'
 
 
@@ -107,3 +107,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
