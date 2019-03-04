c7bb81ad8debaab2c9bbed78dcdb3677e6390ecd
JamesMusyoka <jamesmu475@gmail.com>
2019-03-04 19:59:44 +0300

    urghhhh deploy

--- manage.py
+++ manage.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalG.settings")
     try:
         from django.core.management import execute_from_command_line
-        expect ImportError:
+        except ImportError:
         # Couldn't import Django
 
             try:
                 import django
-            expect ImportError(
+            except ImportError(
                 "Couldn't import Django."
                 )
             raise
