a4441f2baf59651729e603fd4595be667a03dfc2
JamesMusyoka <jamesmu475@gmail.com>
2019-03-04 20:13:58 +0300

    Adding changes urls.py

--- personalG/urls.py
+++ personalG/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^gallery/',include('gallery.urls')),
+    url(r'^$',include('gallery.urls')),
 ]
 
 if settings.DEBUG:
